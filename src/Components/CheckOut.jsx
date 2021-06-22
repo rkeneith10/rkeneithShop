d808a15155a3e3757ba98a40be588d2d4a87e9ec
Your Name <you@example.com>
2021-06-22 15:27:26 -0400

    Changes on several pages

--- src/Components/CheckOut.jsx
+++ src/Components/CheckOut.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import products from "../data/products";
 import { useParams } from "react-router-dom";
 import MC_Button from "../images/MC_button.png";
+import Footer from "./Footer";
 
 import "../css/bootstrap.css";
 import "../css/nav.css";
@@ -14,85 +15,91 @@ function CheckOut() {
     console.log("The Click");
   };
   return (
-    <div className="single">
-      <div className="container">
-        <h3>RKENEITHSHOP Ckeckout</h3>
-        <div className="row">
-          <div className="col-md-8">
-            <div className="checkoutCard">
-              <div className="row">
-                <div className="col-md-4">
-                  <img
-                    src={product.url}
-                    alt=""
-                    style={{ width: "150px", height: "150px" }}
-                  />
-                </div>
-                <div className="col-md-8">
-                  <h5>{product.title}</h5>
-                  <p style={{ fontSize: "13px", color: "#7e7f84" }}>
-                    Price:
-                    <span style={{ fontSize: "19px", color: "#f45a40" }}>
-                      ${product.id * 100}
-                    </span>
-                  </p>
+    <div>
+      <div className="single">
+        <div className="container">
+          <h3>RKENEITHSHOP Ckeckout</h3>
+          <div className="row">
+            <div className="col-md-8">
+              <div className="checkoutCard">
+                <div className="row">
+                  <div className="col-md-4">
+                    <img
+                      src={product.url}
+                      alt=""
+                      style={{ width: "150px", height: "150px" }}
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <h5>{product.title}</h5>
+                    <p style={{ fontSize: "13px", color: "#7e7f84" }}>
+                      Price:
+                      <span style={{ fontSize: "19px", color: "#f45a40" }}>
+                        ${product.id * 100}
+                      </span>
+                    </p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <br />
+              <br />
 
-            <div className="checkoutCard1">
-              <h3>Ship to:</h3>
-              <hr style={{ width: "90%", border: "1px solid #f0f0f0" }} />
+              <div className="checkoutCard1">
+                <h3>Ship to:</h3>
+                <hr style={{ width: "90%", border: "1px solid #f0f0f0" }} />
 
-              <form>
-                <div className="form-group">
-                  <div className="col-md-6 btm">
-                    <label>First Name</label>
-                    <input className="form-control" placeholder="First Name" />
+                <form>
+                  <div className="form-group">
+                    <div className="col-md-6 btm">
+                      <label>First Name</label>
+                      <input
+                        className="form-control"
+                        placeholder="First Name"
+                      />
+                    </div>
+                    <div className="col-md-6 btm">
+                      <label>Last Name</label>
+                      <input className="form-control" placeholder="Last Name" />
+                    </div>{" "}
+                    <div className="col-md-6 btm">
+                      <label>Email</label>
+                      <input className="form-control" placeholder="Email" />
+                    </div>
+                    <div className="col-md-6 btm">
+                      <label>Confirm Email</label>
+                      <input
+                        className="form-control"
+                        placeholder="Confirm Email"
+                      />
+                    </div>
+                    <div className="col-md-6 btm">
+                      <label>Adress</label>
+                      <input className="form-control" placeholder="Adress" />
+                    </div>
+                    <div className="col-md-6 btm">
+                      <label>Phone Number</label>
+                      <input
+                        className="form-control"
+                        type="Number"
+                        placeholder="Phone Number"
+                      />
+                    </div>
+                    <div style={{ marginLeft: "20px" }}>
+                      <img
+                        onClick={() => theclick()}
+                        src={MC_Button}
+                        alt=""
+                        style={{ width: "90px", height: "35px" }}
+                      />
+                    </div>
                   </div>
-                  <div className="col-md-6 btm">
-                    <label>Last Name</label>
-                    <input className="form-control" placeholder="Last Name" />
-                  </div>{" "}
-                  <div className="col-md-6 btm">
-                    <label>Email</label>
-                    <input className="form-control" placeholder="Email" />
-                  </div>
-                  <div className="col-md-6 btm">
-                    <label>Confirm Email</label>
-                    <input
-                      className="form-control"
-                      placeholder="Confirm Email"
-                    />
-                  </div>
-                  <div className="col-md-6 btm">
-                    <label>Adress</label>
-                    <input className="form-control" placeholder="Adress" />
-                  </div>
-                  <div className="col-md-6 btm">
-                    <label>Phone Number</label>
-                    <input
-                      className="form-control"
-                      type="Number"
-                      placeholder="Phone Number"
-                    />
-                  </div>
-                  <div style={{ marginLeft: "20px" }}>
-                    <img
-                      onClick={() => theclick()}
-                      src={MC_Button}
-                      alt=""
-                      style={{ width: "90px", height: "35px" }}
-                    />
-                  </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
+            <div className="col-md-4"></div>
           </div>
-          <div className="col-md-4"></div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
