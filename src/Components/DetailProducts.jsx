a211f990dc051320c56f323adcbf1133ae33bbe6
Your Name <you@example.com>
2021-06-22 00:05:06 -0400

    Changes on several pages

--- src/Components/DetailProducts.jsx
+++ src/Components/DetailProducts.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import products from "../data/products";
 import Footer from "./Footer";
 import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../css/bootstrap.css";
 import "../css/nav.css";
@@ -30,15 +31,29 @@ const DetailProducts = (props) => {
               <div className="single-left-right">
                 <div className="single-left-info" style={{ marginTop: "30px" }}>
                   <h3>{product.title}</h3>
-                  <p className="detailsparagraph">{product.description}</p>
-
+                  <p className="detailsparagraph">
+                    {product.description}
+                    {product.description}
+                    {product.description}
+                    {product.description}
+                    {product.description}
+                    {product.description}
+                    {product.description}
+                    {product.description}
+                  </p>
                   <p style={{ fontSize: "13px", color: "#7e7f84" }}>
                     Price:
                     <span style={{ fontSize: "19px", color: "#f45a40" }}>
                       ${product.id * 100}
                     </span>
                   </p>
-                  <button className="btn btn-keneith">Add to cart</button>
+
+                  <div className="col-md-3">
+                    <div className="d-flex justify-content-between">
+                      <button className="btn btn-keneith">Add to cart</button>
+                      <button className="btn btn-keneith">Buy Now</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -48,7 +63,7 @@ const DetailProducts = (props) => {
 
           {/* kjsjdsk */}
 
-          <div className="related" style={{ marginTop: "22px" }}>
+          <div className="related" style={{ marginTop: "30px" }}>
             <p
               style={{ color: "#f45a40", fontWeight: "bold", fontSize: "20px" }}
             >
@@ -57,27 +72,34 @@ const DetailProducts = (props) => {
             <div className="related-grids">
               {relatedProduct.splice(0, 5).map((r, index) => (
                 <div className="related-grid" key={index}>
-                  <div className="col-md-9 related-left">
-                    <div className="col-md-3 related-left-left">
-                      <img src={r.url} alt="" />
-                    </div>
+                  <div className="col-md-9 ">
+                    <Link to={`/product/${r.id}`}>
+                      <div className="col-md-3 related-left-left">
+                        <img src={r.url} alt="" />
+                      </div>
+                    </Link>
                     <div className="col-md-9 related-left-right">
                       <p style={{ color: "#f45a40", fontSize: "20px" }}>
                         {r.title}
                       </p>
-                      <p>{r.description}</p>
+                      <p>
+                        {r.description}
+                        {r.description}
+                        {r.description}
+                        {r.description}
+                        {r.description}
+                      </p>
+
+                      <p style={{ fontSize: "13px", color: "#7e7f84" }}>
+                        Price:
+                        <span style={{ fontSize: "19px", color: "#f45a40" }}>
+                          ${r.id * 100}
+                        </span>
+                      </p>
                     </div>
                     <div className="clearfix"> </div>
                   </div>
-                  <div className="col-md-3 related-right">
-                    <p style={{ fontSize: "13px", color: "#7e7f84" }}>
-                      Price:
-                      <span style={{ fontSize: "19px", color: "#f45a40" }}>
-                        ${r.id * 100}
-                      </span>
-                    </p>
-                    <button className="btn btn-keneith">Add to cart</button>
-                  </div>
+
                   <div class="clearfix"> </div>
                 </div>
               ))}
